8c432609342d0d2b01aa610a7bea12d5ddb29cd5
Felipe Souza <felipesouzadsgn@gmail.com>
2022-11-08 15:40:08 -0300

    Updating code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,28 @@
 SVGInject(document.querySelectorAll("img.svg"));
+
+const heroCircleMd = document.querySelector(".hero__circle-md")
+const heroCircleLg = document.querySelector(".hero__circle-lg")
+const heroCircleSm = document.querySelector(".hero__circle-sm")
+
+const heroSocialSm = document.querySelector(".hero__social-sm")
+const heroSocialMd = document.querySelector(".hero__social-md")
+const heroSocialLg = document.querySelector(".hero__social-lg")
+
+if (heroCircleSm) {
+    heroCircleSm.addEventListener("click", () => {
+        heroSocialLg.classList.remove("show")
+        heroSocialMd.classList.remove("show")
+        heroCircleLg.classList.toggle("show")
+        heroCircleMd.classList.toggle("show")
+    })
+}
+
+
+if (heroSocialSm) {
+    heroSocialSm.addEventListener("click", () => {
+        heroCircleLg.classList.remove("show")
+        heroCircleMd.classList.remove("show")
+        heroSocialLg.classList.toggle("show")
+        heroSocialMd.classList.toggle("show")
+    })
+}
